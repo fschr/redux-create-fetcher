4c3f6b59af3aac61198642e8ec9d678a09a5be68
Thomas Fischer <thomasfischer@fastmail.com>
2018-03-23 20:54:49 -0500

    I have no idea what I'm doing

--- index.js
+++ index.js
@@ -1,67 +1,63 @@
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 const Fetcher = (actionPrefix, {
     fetchURLFunc,
     fetchOptionsFunc = key => ({ method: 'GET' }),
     responseType = 'json',
     parseResponse = value => value,
-    verbose = false,
+    verbose = false
 }) => {
 
     const requestAction = key => ({
         type: actionPrefix + '_REQUEST',
         key
-    })
+    });
 
     const successAction = ({ key, result }) => ({
         type: actionPrefix + '_SUCCESS',
         key,
         result
-    })
+    });
 
     const failureAction = ({ key, error }) => ({
         type: actionPrefix + '_FAILURE',
         key,
         error
-    })
-
+    });
 
     return key => {
         return dispatch => {
 
-            const dispatchSuccessAction = stuff => dispatch(successAction({ ...stuff, key }))
-            const dispatchFailureAction = stuff => dispatch(failureAction({ ...stuff, key }))
+            const dispatchSuccessAction = stuff => dispatch(successAction(_extends({}, stuff, { key })));
+            const dispatchFailureAction = stuff => dispatch(failureAction(_extends({}, stuff, { key })));
 
-            if (typeof actionPrefix !== 'string' ||
-                typeof fetchURLFunc !== 'function') {
-                console.error('Fetcher requires string `actionPrefix` and ' +
-                              'function `fetchURLFunc` to be specified in ' +
-                              'paramsFunc; got ' + actionPrefix + ' (type ' +
-                              (typeof actionPrefix) + ') and (type ' +
-                              (typeof fetchURLFunc) + '), respectively')
+            if (typeof actionPrefix !== 'string' || typeof fetchURLFunc !== 'function') {
+                console.error('Fetcher requires string `actionPrefix` and ' + 'function `fetchURLFunc` to be specified in ' + 'paramsFunc; got ' + actionPrefix + ' (type ' + typeof actionPrefix + ') and (type ' + typeof fetchURLFunc + '), respectively');
                 return;
             }
 
-	    dispatch(requestAction(key));
+            dispatch(requestAction(key));
 
-	    return fetch(fetchURLFunc(key), fetchOptionsFunc(key))
-                .then(response => {
-                    value: response[responseType](),
-                    status: response.status,
-                    statusText: response.statusText,
-                    ok: response.ok
-                })
-                .then({ value, ...stuff } => {
-                    let result = parseResponse(value);
-                    if (typeof result.then === 'function') {
-                        result
-                            .then(result => dispatchSuccessAction({ ...stuff, result }))
-                            .catch(error => dispatchFailureAction({ error: error.toString(), ...stuff }))
-                    } else {
-                        dispatchSuccessAction({ ...stuff, result })
-                    }
-                })
-                .catch(error => dispatchFailureAction({ error: error.toString() }))
-        }
-    }
-}
+            return fetch(fetchURLFunc(key), fetchOptionsFunc(key)).then(response => ({
+                value: response[responseType](),
+                status: response.status,
+                statusText: response.statusText,
+                ok: response.ok
+            })).then(_ref => {
+                let { value } = _ref,
+                    stuff = _objectWithoutProperties(_ref, ['value']);
+
+                let result = parseResponse(value);
+                if (typeof result.then === 'function') {
+                    result.then(result => dispatchSuccessAction(_extends({}, stuff, { result }))).catch(error => dispatchFailureAction(_extends({ error: error.toString() }, stuff)));
+                } else {
+                    dispatchSuccessAction(_extends({}, stuff, { result }));
+                }
+            }).catch(error => dispatchFailureAction({ error: error.toString() }));
+        };
+    };
+};
 
 export default Fetcher;
---
+++ lib.js
@@ -0,0 +1,67 @@
+const Fetcher = (actionPrefix, {
+    fetchURLFunc,
+    fetchOptionsFunc = key => ({ method: 'GET' }),
+    responseType = 'json',
+    parseResponse = value => value,
+    verbose = false,
+}) => {
+
+    const requestAction = key => ({
+        type: actionPrefix + '_REQUEST',
+        key
+    })
+
+    const successAction = ({ key, result }) => ({
+        type: actionPrefix + '_SUCCESS',
+        key,
+        result
+    })
+
+    const failureAction = ({ key, error }) => ({
+        type: actionPrefix + '_FAILURE',
+        key,
+        error
+    })
+
+
+    return key => {
+        return dispatch => {
+
+            const dispatchSuccessAction = stuff => dispatch(successAction({ ...stuff, key }))
+            const dispatchFailureAction = stuff => dispatch(failureAction({ ...stuff, key }))
+
+            if (typeof actionPrefix !== 'string' ||
+                typeof fetchURLFunc !== 'function') {
+                console.error('Fetcher requires string `actionPrefix` and ' +
+                              'function `fetchURLFunc` to be specified in ' +
+                              'paramsFunc; got ' + actionPrefix + ' (type ' +
+                              (typeof actionPrefix) + ') and (type ' +
+                              (typeof fetchURLFunc) + '), respectively')
+                return;
+            }
+
+	    dispatch(requestAction(key));
+
+	    return fetch(fetchURLFunc(key), fetchOptionsFunc(key))
+                .then(response => ({
+                    value: response[responseType](),
+                    status: response.status,
+                    statusText: response.statusText,
+                    ok: response.ok
+                }))
+                .then( ({ value, ...stuff }) => {
+                    let result = parseResponse(value);
+                    if (typeof result.then === 'function') {
+                        result
+                            .then(result => dispatchSuccessAction({ ...stuff, result }))
+                            .catch(error => dispatchFailureAction({ error: error.toString(), ...stuff }))
+                    } else {
+                        dispatchSuccessAction({ ...stuff, result })
+                    }
+                })
+                .catch(error => dispatchFailureAction({ error: error.toString() }))
+        }
+    }
+}
+
+export default Fetcher;
